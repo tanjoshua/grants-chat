f11b92c55b613b9fcc29d73b1cbec38f8b0f2798
Joshua TAN <joshua_tan@tech.gov.sg>
2025-02-14 14:46:09 +0800

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+
   /* config options here */
 };
 
