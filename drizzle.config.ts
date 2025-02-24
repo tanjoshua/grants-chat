92621eaccd0ee96febce5f75b3130539939b0646
Joshua TAN <joshua_tan@tech.gov.sg>
2025-02-24 16:15:26 +0800

    update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,4 @@
+import '@/config/env';
 import type { Config } from 'drizzle-kit';
 
 export default {
@@ -5,7 +6,7 @@ export default {
   out: './src/db/migrations',
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5432/rag_dev'
+    url: process.env.DATABASE_URL!
   },
   verbose: true,
   strict: true
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,16 @@
-// import { neon, neonConfig } from '@neondatabase/serverless';
-// import { drizzle } from 'drizzle-orm/neon-http';
-// import * as schema from './schema';
-
-// const sql = neon(process.env.DATABASE_URL!);
-
-// export const db = drizzle(sql, { schema });
-import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(sql, { schema });
+// import { Pool } from 'pg';
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import * as schema from './schema';
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export const db = drizzle(pool, { schema });
