f18ec0e3df4b97e73e31ea9dbe8a46b3f5060434
Joshua TAN <joshua_tan@tech.gov.sg>
2025-02-25 11:33:50 +0800

    csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -54,12 +54,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    {
-      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
-    },
+   
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
