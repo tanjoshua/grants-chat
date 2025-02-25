80e3f3c79c3f160c4d3121cf5b4b2267c2915c1c
Joshua TAN <joshua_tan@tech.gov.sg>
2025-02-25 12:31:54 +0800

    trying something else

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Chat } from '@/components/chat';
 
+export const dynamic = 'force-dynamic';
+
 export default function Page() {
   return (
     <div className="flex-1 flex flex-col max-w-4xl mx-auto w-full">
